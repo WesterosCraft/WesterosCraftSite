e8ad0850fceab137e77852c525e3bfa7c65b5271
Jacob Granberry <jacobgranberry@gmail.com>
2019-08-18 14:23:47 -0500

    init

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Prismic from 'prismic-javascript'
+import { RichText, Date } from 'prismic-reactjs'
+import { client } from '../prismic-configuration'
+
+const HomePage = props => (
+  <div>
+    <h1>hello</h1>
+  </div>
+)
+
+HomePage.getInitialProps = async context => {
+  const home = await client.getSingle('home_page')
+
+  return { home }
+}
+
+export default HomePage
