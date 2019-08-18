8b71712021ca0dd4f49a50ac98ca6581d28e7adf
Jacob Granberry <jacobgranberry@gmail.com>
2019-08-18 16:33:19 -0500

    styled components

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,26 @@
-import React from "react";
-import { RichText } from "prismic-reactjs";
-import { Text } from "rebass";
-import { client } from "../prismic-configuration";
+import React from 'react';
+import { RichText } from 'prismic-reactjs';
+import { Text } from 'rebass';
+import { client } from '../prismic-configuration';
 
 const HomePage = props => {
   const { data } = props.home;
 
   return (
     <div>
-      <Text color="brand_red" variant='paragraph'>{RichText.asText(data.heading)}</Text>
+      <Text
+        sx={{
+          color: 'brand_red',
+        }}
+      >
+        {RichText.asText(data.heading)}
+      </Text>
     </div>
   );
 };
 
 HomePage.getInitialProps = async context => {
-  const home = await client.getSingle("home_page");
+  const home = await client.getSingle('home_page');
 
   return { home };
 };
--- theme/theme.js
+++ theme/theme.js
@@ -1,25 +1,27 @@
 /* eslint-disable import/prefer-default-export */
-export const theme = {
-  breakpoints: ["32em", "48em", "64em", "75em"],
+const theme = {
+  breakpoints: ['32em', '48em', '64em', '75em'],
   space: [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 48, 56, 64, 72, 80, 96],
   fontSizes: [8, 12, 14, 16, 18, 20, 24, 28, 32, 36, 40, 44, 48],
   colors: {
-    white: "#FFFFFF",
-    brand_red: "#9B1313",
-    brand_yellow: "",
-    brand_blue: "",
-    text_color: "red",
-    background_color: ""
+    white: '#FFFFFF',
+    brand_red: '#9B1313',
+    brand_yellow: '',
+    brand_blue: '',
+    text_color: 'red',
+    background_color: '',
   },
   font: {
-    primary_font: "",
-    secondary_font: ""
+    primary_font: '',
+    secondary_font: '',
   },
   text: {
     paragraph: {
       fontSize: 6,
-      color: "brand_red",
-      p: 4
-    }
-  }
+      color: 'brand_red',
+      p: 4,
+    },
+  },
 };
+
+export default theme;
