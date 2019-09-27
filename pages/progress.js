09d4d2a81ef7f0b333f0d140b62fa75e48e5986f
Jacob Granberry <jacobgranberry@gmail.com>
2019-09-26 19:53:44 -0500

    removes destructure

--- pages/progress.js
+++ pages/progress.js
@@ -6,12 +6,7 @@ import moreLocations from '../queries/moreLocations.graphql';
 import LocationTable from '../components/organisms/locationTable/locationTable';
 
 const ProgressPage = () => {
-  const {
-    loading,
-    error,
-    data: { allLocation_wiki_entrys },
-    fetchMore,
-  } = useQuery(locationsQuery);
+  const { loading, error, data, fetchMore } = useQuery(locationsQuery);
 
   if (loading) return <h1> Loading... </h1>;
   if (error) return <h1> error! </h1>;
@@ -26,22 +21,22 @@ const ProgressPage = () => {
         bg="white"
         mt={200}
       >
-        <LocationTable pages={allLocation_wiki_entrys.edges || []} />{' '}
+        <LocationTable pages={data.allLocation_wiki_entrys.edges || []} />{' '}
         <Button
           onClick={() =>
             fetchMore({
               query: moreLocations,
               variables: {
-                cursor: allLocation_wiki_entrys.pageInfo.endCursor,
+                cursor: data.allLocation_wiki_entrys.pageInfo.endCursor,
               },
               updateQuery: (previousResult, { fetchMoreResult }) => {
-                const newEdges = fetchMoreResult.allLocation_wiki_entrys.edges;
+                const newEdges = fetchMoreResult.data.allLocation_wiki_entrys.edges;
                 const { pageInfo } = fetchMoreResult.allLocation_wiki_entrys;
 
                 return newEdges.length
                   ? {
                       allLocation_wiki_entrys: {
-                        __typename: previousResult.allLocation_wiki_entrys.__typename,
+                        __typename: previousResult.data.allLocation_wiki_entrys.__typename,
                         edges: [...previousResult.allLocation_wiki_entrys.edges, ...newEdges],
                         pageInfo,
                       },
