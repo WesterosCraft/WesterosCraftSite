f8994fd9a1245058a1fe6bfed2ad129cf067dde7
Jacob Granberry <jacob.granberry@company-corp.com>
2019-12-25 21:37:48 -0700

    idk

--- components/templates/wikiPage/wikiPage.jsx
+++ components/templates/wikiPage/wikiPage.jsx
@@ -4,11 +4,27 @@ import { Box, Text, Flex } from 'rebass';
 const WikiPage = ({ data }) => {
   console.log('wikie page data: ', data);
   return (
-    <Box mt={160}>
-      <Box className="wiki-container">
+    <Box>
+      <Box className="wiki-container" pt={160}>
         <Flex className="wiki-main">
-          <Flex as="aside" className="wiki-nav" />
-          <Box className="wiki-content-container">
+          <Flex as="aside" className="wiki-nav" width={250} pr={5}>
+            <Box
+              sx={{
+                position: 'sticky',
+                top: '0.75rem',
+                height: 'calc(100vh - 0.75rem)',
+                paddingBottom: '2rem',
+                overflowY: 'auto',
+              }}
+            />
+          </Flex>
+          <Box
+            className="wiki-content-container"
+            width="auto"
+            sx={{
+              boxShadow: 'rgba(0, 0, 0, 0.08) 0px 4px 8px 0px',
+            }}
+          >
             <Box className="wiki-content">
               <Box className="content-wrapper" />
               <Box className="content-sidebar" />
