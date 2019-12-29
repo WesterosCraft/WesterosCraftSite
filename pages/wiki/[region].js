148715e38e0b63b6f95ad6c0d326af42a5a0a6a1
Jacob Granberry <jacob.granberry@company-corp.com>
2019-12-29 12:52:31 -0700

    region page and query

--- pages/wiki/[region].js
+++ pages/wiki/[region].js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import Loader from '../../components/atoms/loader/loader';
 import Layout from '../../components/templates/layout/layout';
+import WikiPage from '../../components/templates/wikiPage/wikiPage';
+import regionQuery from '../../queries/region.graphql';
 
-const Region = () => {
+const Region = ({ region }) => {
+  const { loading, error, data } = useQuery(regionQuery, {
+    variables: {
+      region,
+    },
+  });
+
+  console.log('REGION DATA: ', data);
+
+  if (error) {
+    console.log(error);
+  }
+
+  if (loading) return <Loader />;
+
   return (
     <Layout>
       <h1>hi</h1>{' '}
     </Layout>
   );
+};
+
+Region.getInitialProps = async ({ query }) => {
+  return query;
 };
 
 export default Region;
