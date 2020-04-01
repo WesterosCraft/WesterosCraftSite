a84935ea946619cb93c20d9de17cd53c4685153e
Jacob <jacobgranberry@gmail.com>
2020-03-31 22:34:16 -0500

    format

--- src/components/atoms/redactor/redactor.js
+++ src/components/atoms/redactor/redactor.js
@@ -1,91 +1,102 @@
-import styled from '@emotion/styled';
+import styled from '@emotion/styled'
 
 export const Redactor = styled.div`
+  /* GLOBAL STYLES */
 
-/* GLOBAL STYLES */
-
-h1, h2, h3, h4, h5, h6, p, a {
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  a {
     line-height: 1.25;
     font-weight: normal;
     margin-block-start: 0;
     margin-block-end: 0;
-}
+  }
 
-h1, h2, h3, h4, h5, h6 {
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
     font-family: 'Sen', sans-serif;
-}
+  }
 
-p, a {
+  p,
+  a {
     font-family: 'Roboto', sans-serif;
-}
+  }
 
-h3 + h4 {
+  h3 + h4 {
     margin-top: 22px;
-}
+  }
 
-/* MOBILE STYLES */
+  /* MOBILE STYLES */
 
-h1 {
+  h1 {
     font-size: 36px;
     font-weight: 700;
-}
+  }
 
-h2 {
+  h2 {
     font-size: 32px;
     font-weight: 700;
-}
+  }
 
-h3 {
+  h3 {
     font-size: 24px;
-}
+  }
 
-h4 {
+  h4 {
     font-size: 20px;
     font-weight: 400;
-}
+  }
 
-h5 {
+  h5 {
     font-size: 18px;
     font-weight: 400;
-}
+  }
 
-h6 {
+  h6 {
     font-size: 16px;
-}
+  }
 
-p {
+  p {
     font-size: 16px;
-}
+  }
 
-a {
+  a {
     font-size: 16px;
     text-decoration: none;
     font-weight: bold;
-    color: #9E1E22;
-}
+    color: #9e1e22;
+  }
 
-a:hover {
-    color: #B32227;
-}
+  a:hover {
+    color: #b32227;
+  }
 
-/* TABLET STYLES */
+  /* TABLET STYLES */
 
-
-@media screen and (min-width: 512px) {
+  @media screen and (min-width: 512px) {
     h1 {
-        font-size: 48px;
+      font-size: 48px;
     }
 
     h2 {
-        font-size: 36px;
+      font-size: 36px;
     }
 
     h3 {
-        font-size: 32px;
+      font-size: 32px;
     }
 
     h4 {
-        font-size: 24px;
+      font-size: 24px;
     }
 
     h5 {
@@ -99,44 +110,41 @@ a:hover {
 
     a {
     }
+  }
 
-}
+  /* LAPTOP STYLES */
 
-/* LAPTOP STYLES */
-
-
-@media screen and (min-width: 768px) {
-     h1 {
-        font-size: 64px;
+  @media screen and (min-width: 768px) {
+    h1 {
+      font-size: 64px;
     }
 
-     h2 {
-        font-size: 48px;
+    h2 {
+      font-size: 48px;
     }
 
-     h3 {
-        font-size: 36px;
+    h3 {
+      font-size: 36px;
     }
 
-     h4 {
-        font-size: 32px;
+    h4 {
+      font-size: 32px;
     }
 
-     h5 {
-         font-size: 24px;
-     }
+    h5 {
+      font-size: 24px;
+    }
 
-     h6 {
-         font-size: 20px;
-     }
+    h6 {
+      font-size: 20px;
+    }
 
-     p {
-         font-size: 16px;
-     }
+    p {
+      font-size: 16px;
+    }
 
-     a {
-         font-size: 16px;
-     }
-
-}
+    a {
+      font-size: 16px;
+    }
+  }
 `
