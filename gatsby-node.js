0f2846effcc2fbea72223d0ab9d8ba3cf8cca035
Jacob Granberry <jacob.granberry@company-corp.com>
2020-04-07 23:46:55 -0500

    dynamic pages for wiki destinations

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,39 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  const result = await graphql(
+    `
+      {
+        craft {
+          entries(site: "westeroscraft", type: "wikiDestination") {
+            ... on Craft_wiki_wikiDestination_Entry {
+              title
+              slug
+              projectDetails {
+                ... on Craft_projectDetails_details_BlockType {
+                  region
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+  )
+  if (result.errors) {
+    reporter.panicOnBuild('Error while running GraphQL query.')
+    return
+  }
+
+  const entries = result.data.craft.entries
+
+  entries.forEach((entry, i) => {
+    createPage({
+      path: `/wiki/${entry.projectDetails[0].region}/${entry.slug}`,
+      component: path.resolve('./src/pages/destination.js'),
+      context: {
+        test: entry,
+      },
+    })
+  })
