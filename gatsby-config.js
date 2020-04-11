e6bb9bf323328b614a52ad0d8722fbc25e0de321
Jacob <jacobgranberry@gmail.com>
2020-04-11 11:56:58 -0500

    removes algoolia

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,5 @@
 require('dotenv').config()
 
-const queries = require('./src/utility/algolia')
-
 module.exports = {
   siteMetadata: {
     title: `WesterosCraft`,
@@ -15,15 +13,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-layout`,
-    {
-      resolve: `gatsby-plugin-algolia`,
-      options: {
-        appId: process.env.GATSBY_ALGOLIA_APP_ID,
-        apiKey: process.env.ALGOLIA_ADMIN_KEY,
-        queries,
-        chunkSize: 10000, // default: 1000
-      },
-    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/components/organisms/wikiNav/wikiNav.jsx
+++ src/components/organisms/wikiNav/wikiNav.jsx
@@ -3,7 +3,6 @@ import { Box, Text, Flex } from 'rebass'
 import { Input } from '@rebass/forms'
 import { Link } from 'gatsby'
 import { IoMdArrowDropdown } from 'react-icons/io'
-import Search from '../search'
 
 const WikiNavGroup = ({ navItem }) => {
   const [open, setOpen] = useState(true)
@@ -75,8 +74,7 @@ export const WikiNav = ({ navData }) => {
       maxWidth={['100%', null, 320]}
       sx={{ position: 'relative' }}
     >
-      <Search collapse indices={searchIndices} />
-      {/* <Input placeholder="Search Wiki" mb="44px" /> */}
+      <Input placeholder="Search Wiki" mb="44px" />
       <Box
         sx={{
           display: ['none', null, 'block'],
