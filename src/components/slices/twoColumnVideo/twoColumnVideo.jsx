ecd8f068522673e9f24b28ac1071543a4b8bc21c
Jacob Granberry <jacob.granberry@company-corp.com>
2020-04-26 16:38:58 -0500

    vudei

--- src/components/slices/twoColumnVideo/twoColumnVideo.jsx
+++ src/components/slices/twoColumnVideo/twoColumnVideo.jsx
@@ -4,7 +4,6 @@ import { TwoColumnLayout } from '../../organisms/twoColumnLayout';
 import { VideoEmbed } from '../../organisms/videoEmbed';
 import { Redactor } from '../../atoms/redactor/redactor';
 import ScrollAnimation from 'react-animate-on-scroll';
-import { Doodle } from '../../atoms/doodle';
 
 export const TwoColumnVideo = ({ data }) => (
   <Box className="two-column-video">
@@ -22,26 +21,6 @@ export const TwoColumnVideo = ({ data }) => (
         )}
       </TwoColumnLayout.ColumnOne>
       <TwoColumnLayout.ColumnTwo ml={0} sx={{ position: 'relative' }}>
-        <Box
-          className="doodle-container"
-          width={1}
-          height="100%"
-          sx={{ position: 'absolute', bottom: [-10, null, 0], right: 0 }}
-        >
-          {/* <Doodle
-            rule={`
-            :doodle {
-              @grid: 18 / 100%;
-              background: #fff;
-            }
-            background: rgba(
-              80, 86, 82,
-              calc(@row * @col / @size)
-            );
-            transform: scale(@rand(.2, .5));
-        `}
-          /> */}
-        </Box>
         <ScrollAnimation animateIn="fadeInRight" animateOnce>
           <VideoEmbed data={data.children[1]} p={0} />
         </ScrollAnimation>
