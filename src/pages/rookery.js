a91a55dca6e8e9eaed18de147408abcc17ba85ff
Jacob Granberry <jacob.granberry@company-corp.com>
2020-04-27 22:02:15 -0500

    pdf not owrking

--- src/pages/rookery.js
+++ src/pages/rookery.js
@@ -1,18 +1,52 @@
 import React from 'react';
-import { Heading } from 'rebass';
+
+import { graphql } from 'gatsby';
+import { Heading, Box, Flex, Text } from 'rebass';
+import { Document, Page } from 'react-pdf';
 
 const RookeryPage = ({ data }) => {
+  console.log(data);
   return (
     <>
       <Heading variant="heading2" textAlign="center" mt={[12]}>
-        The Rookery
+        {data.craft.entry.heading}
       </Heading>
-      <Heading>
-        The WesterosCraft newsletter aiming to bring you up to date on all the latest community happenings
-      </Heading>
-      {/* <SliceZone slices={data.craft.entry.pageSlices} /> */}
+      <Heading textAlign="center">{data.craft.entry.subheading}</Heading>
+      <Flex>
+        <Document file="https://westeroscraft.com/web/assets/files/Rookery-Autumn-2019.pdf">
+          <Page pageNumber={1} />
+        </Document>
+        <Box my={86}>
+          {data.craft.entry.rookeryList.map((item) => (
+            <>
+              <Text>{item.rookeryTitle}</Text>
+              <Text>{item.rookeryFile[0].url}</Text>
+            </>
+          ))}
+        </Box>
+      </Flex>
     </>
   );
 };
+export const pageQuery = graphql`
+  query rookeryQuery {
+    craft {
+      entry(site: "westeroscraft", section: "rookery") {
+        ... on Craft_rookery_rookery_Entry {
+          heading
+          subheading
+          rookeryList {
+            ... on Craft_rookeryList_rookery_BlockType {
+              rookeryFile {
+                url
+              }
+              rookeryTitle
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default RookeryPage;
