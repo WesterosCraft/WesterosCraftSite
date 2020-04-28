b19f5b3be48b25a4b475cdfdcbd436baa8e8ac75
Jacob Granberry <jacob.granberry@company-corp.com>
2020-04-27 23:00:58 -0500

    hook

--- src/pages/rookery.js
+++ src/pages/rookery.js
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { graphql } from 'gatsby';
 import { Heading, Box, Flex, Text } from 'rebass';
 import { Document, Page } from 'react-pdf';
 
 const RookeryPage = ({ data }) => {
-  console.log(data);
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess(document) {
+    setNumPages(document.numPages);
+  }
+
   return (
     <>
       <Heading variant="heading2" textAlign="center" mt={[12]}>
         {data.craft.entry.heading}
       </Heading>
-      <Heading textAlign="center">{data.craft.entry.subheading}</Heading>
-      <Flex>
-        <Document file="https://westeroscraft.com/web/assets/files/Rookery-Autumn-2019.pdf">
-          <Page pageNumber={1} />
-        </Document>
-        <Box my={86}>
-          {data.craft.entry.rookeryList.map((item) => (
-            <>
-              <Text>{item.rookeryTitle}</Text>
-              <Text>{item.rookeryFile[0].url}</Text>
-            </>
-          ))}
+      <Heading>{data.craft.entry.subheading}</Heading>
+      <Flex flexDirection="column">
+        <Box>
+          <Document file={data.craft.entry.rookeryList[0].rookeryFile[0].url} onLoadSuccess={onDocumentLoadSuccess}>
+            <Page pageNumber={pageNumber} />
+          </Document>
+          <p>
+            Page {pageNumber} of {numPages}
+          </p>
         </Box>
+        {data.craft.entry.rookeryList.map((item) => (
+          <Box>
+            <Text>{item.rookeryTitle}</Text>
+            <Text>{item.rookeryFile[0].url}</Text>
+          </Box>
+        ))}
       </Flex>
     </>
   );
