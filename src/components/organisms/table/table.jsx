3e746d642570f9ca78bd6d2b425bb23c9e22e516
Jacob <jacobgranberry@gmail.com>
2020-04-28 21:15:00 -0500

    completes rookery page

---
+++ src/components/organisms/table/table.jsx
@@ -0,0 +1,130 @@
+import React from 'react';
+import { useTable, useSortBy, usePagination, useResizeColumns, useFlexLayout, useRowSelect } from 'react-table';
+import { Flex, Box, Text, Button } from 'rebass';
+
+export const Table = ({ columns, data, maxWidth = '100%' }) => {
+  const headerProps = (props, { column }) => getStyles(props, column.align);
+
+  const cellProps = (props, { cell }) => getStyles(props, cell.column.align);
+
+  const getStyles = (props, align = 'left') => [
+    props,
+    {
+      style: {
+        justifyContent: align === 'right' ? 'flex-end' : 'flex-start',
+        alignItems: 'flex-start',
+        display: 'flex',
+      },
+    },
+  ];
+  const defaultColumn = React.useMemo(
+    () => ({
+      // When using the useFlexLayout:
+      minWidth: 30, // minWidth is only used as a limit for resizing
+      width: 150, // width is used for both the flex-basis and flex-grow
+      maxWidth: 200, // maxWidth is only used as a limit for resizing
+    }),
+    [],
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, prepareRow, page } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn,
+    },
+    useSortBy,
+    usePagination,
+    useResizeColumns,
+    useFlexLayout,
+    useRowSelect,
+  );
+
+  return (
+    <Box
+      className="table"
+      maxWidth={maxWidth}
+      mx="auto"
+      mb={140}
+      width={1}
+      sx={{
+        boxShadow: `0 3.9px 3.5px rgba(0, 0, 0, 0.046),
+            0 9.4px 8.4px rgba(0, 0, 0, 0.065),
+            0 17.7px 15.9px rgba(0, 0, 0, 0.073),
+            0 31.5px 28.4px rgba(0, 0, 0, 0.076)
+          `,
+        height: 'auto',
+
+        overflowY: 'hidden',
+        border: '2px solid black',
+      }}
+      {...getTableProps()}
+    >
+      <Box
+        py={5}
+        className="table-content"
+        sx={{
+          overflowY: 'auto',
+          position: 'relative',
+        }}
+      >
+        {headerGroups.map((headerGroup) => (
+          <div className="tr" key={headerGroup.index} {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <Text
+                as="p"
+                fontWeight="bold"
+                fontSize="14px"
+                ml={4}
+                sx={{
+                  textTransform: 'uppercase',
+                }}
+                pb={4}
+                key={column.index}
+                {...column.getHeaderProps(headerProps)}
+                className="th"
+              >
+                {column.render('Header')}
+              </Text>
+            ))}
+          </div>
+        ))}
+        <Box
+          className="table-items tbody"
+          sx={{ border: 'none', borderTop: '1px solid black' }}
+          {...getTableBodyProps()}
+        >
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <Flex
+                className="tr"
+                flexDirection="row"
+                alignItems="center"
+                justifyContent="space-around"
+                key={row.index}
+                width={1}
+                py={1}
+                sx={{
+                  borderBottom: '1px solid #e2e8f0',
+                  minHeight: '48px',
+                }}
+                {...row.getRowProps()}
+              >
+                {row.cells.map((cell, i) => {
+                  return (
+                    <Box width={1} px={4} key={i} {...cell.getCellProps(cellProps)} className="td">
+                      <Text as="span" fontSize="14px">
+                        {cell.render('Cell')}
+                      </Text>
+                    </Box>
+                  );
+                })}
+              </Flex>
+            );
+          })}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
--- src/pages/rookery.js
+++ src/pages/rookery.js
@@ -1,24 +1,54 @@
-import React, { useState } from 'react';
+import React, { useMemo } from 'react';
 
 import { graphql } from 'gatsby';
-import { Heading, Box, Flex, Text } from 'rebass';
+import { Heading, Box, Flex, Text, Image } from 'rebass';
 import Iframe from 'react-iframe';
-
-import { Spinner } from '../components/atoms/spinner';
+import { Table } from '../components/organisms/table';
 
 const RookeryPage = ({ data }) => {
-  console.log(data);
+  const columns = useMemo(
+    () => [
+      {
+        accessor: 'rookeryTitle',
+        Header: 'Rookery Title',
+        filterable: false,
+      },
+      {
+        accessor: 'rookeryUrl',
+        Header: 'Rookery Link',
+        filterable: false,
+        Cell: ({ row }) => (
+          <a href={row.original.rookeryUrl} target="_blank" rel="noopener noreferrer">
+            {row.original.rookeryUrl}
+          </a>
+        ),
+      },
+    ],
+    [],
+  );
+
+  const rookeryData = useMemo(() => data.craft.entry.rookeryList, [data.craft.entry.rookeryList]);
 
   return (
     <>
-      <Heading variant="heading2" textAlign="center" mt={[12]} px={5}>
-        {data.craft.entry.heading}
-      </Heading>
-      <Heading variant="heading4" textAlign="center" maxWidth={786} mx="auto" px={5}>
-        {data.craft.entry.subheading}
-      </Heading>
-      <Flex>
-        <Box width={1} maxWidth={1120} height={792} my={10} mx="auto">
+      <Flex width={1} justifyContent="center" flexDirection="column">
+        <Heading variant="heading2" textAlign="center" mt={[12]} px={5}>
+          {data.craft.entry.heading}
+        </Heading>
+        <Heading variant="heading4" textAlign="center" maxWidth={786} mx="auto" px={5} mt={4}>
+          {data.craft.entry.subheading}
+        </Heading>
+        <Image
+          mt={4}
+          src="https://westeroscraft.com/web/assets/images/crow-icon.png"
+          width="40px"
+          alt="crow"
+          mx="auto"
+        />
+      </Flex>
+
+      <Flex flexDirection="column" mb={17}>
+        <Box width={1} maxWidth={1256} height={792} my={10} mx="auto">
           <Iframe
             url={data.craft.entry.rookeryList[0].rookeryUrl}
             width="100%"
@@ -29,6 +59,12 @@ const RookeryPage = ({ data }) => {
             allowFullScreen
           />
         </Box>
+      </Flex>
+      <Flex width={1} flexDirection="column" maxWidth={1120} justifyContent="center" mx="auto">
+        <Text variant="heading4" as="h4" mb={5}>
+          Previous Rookery Editions
+        </Text>
+        <Table columns={columns} data={rookeryData} />
       </Flex>
     </>
   );
