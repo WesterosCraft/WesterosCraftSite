906f105a6cf036bf457f781e07e9874496c51e3f
Jacob <jacobgranberry@gmail.com>
2020-05-08 18:00:02 -0500

    removes sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-layout`,
-    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-algolia`,
       options: {
