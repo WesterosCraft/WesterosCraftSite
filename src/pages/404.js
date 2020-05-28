a3a04f8ef0dda104d2f12eaa3804b9600dd0e11d
Jacob Granberry <jacob.granberry@company-corp.com>
2020-05-27 22:34:27 -0500

    Adds 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Text, Flex, Heading } from 'rebass';
+import { Button } from '../components/atoms/button';
 
 const NotFoundPage = () => (
-  <>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
-)
+  <Flex width={1} justifyContent="center" alignItems="center" flexDirection="column" px={5} my={[180, 300]}>
+    <Heading variant="heading1" as="h1" textAlign="center">
+      PAGE NOT FOUND
+    </Heading>
+    <Text mt={5} variant="heading6" textAlign="center">
+      Uh oh...you've reached the end of the map! Don't sail into the west just yet.
+    </Text>
+    <Button mt={12} variant="red" href="/">
+      Go Home
+    </Button>
+  </Flex>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
