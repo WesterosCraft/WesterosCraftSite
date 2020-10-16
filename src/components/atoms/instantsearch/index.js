2985bf89c7613e27d39f2db6436a2b2682407bb6
Jacob <jacobgranberry@gmail.com>
2020-10-15 20:42:51 -0500

    algolia

---
+++ src/components/atoms/instantsearch/index.js
@@ -0,0 +1,10 @@
+import { findResultsState } from 'react-instantsearch-dom/server';
+import algoliasearch from 'algoliasearch/lite';
+
+const indexName = 'Wiki';
+
+// Keys are supplied from Algolio's instant search example
+// https://github.com/algolia/react-instantsearch
+const searchClient = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76');
+
+export { findResultsState, indexName, searchClient };
