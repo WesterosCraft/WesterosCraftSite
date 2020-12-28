7fa9c37de44f4eadaa200cf7038dfa015d3ee845
Jacob Granberry <jacob.granberry@company-corp.com>
2020-12-27 21:53:23 -0600

    comment webpack

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-module.exports = {
-  webpack: (config, {}) => {
-    config.module.rules.push({
-      test: /\.(graphql|gql)$/,
-      exclude: /node_modules/,
-      loader: 'graphql-tag/loader'
-    });
-    return config;
-  },
-  images: {
-    domains: ['cdn.westeroscraft.com']
-  }
-};
+// module.exports = {
+//   webpack: (config, {}) => {
+//     config.module.rules.push({
+//       test: /\.(graphql|gql)$/,
+//       exclude: /node_modules/,
+//       loader: 'graphql-tag/loader'
+//     });
+//     return config;
+//   },
+//   images: {
+//     domains: ['cdn.westeroscraft.com']
+//   }
+// };
