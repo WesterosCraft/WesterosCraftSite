1d8b3cce61176acb28133e188e732a26ef0417d5
Jacob Granberry <jacob.granberry@company-corp.com>
2021-02-03 23:21:24 -0600

    ATX-1327 | nav icons

--- components/dropdown/index.js
+++ components/dropdown/index.js
@@ -1,10 +1,29 @@
 import React, { useState, useRef, useEffect } from 'react';
+import { FaCrow, FaDiscord, FaInfoCircle, FaPencilAlt, FaTshirt } from 'react-icons/fa';
+import { IoMdChatbubbles } from 'react-icons/io';
+import { RiHandCoinFill } from 'react-icons/ri';
+import { GiStoneBlock } from 'react-icons/gi';
 
-const DropdownItem = ({ children = '', href = '' }) => (
-  <a
-    className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-    href={href}>
-    {children}
+const iconProps = 'flex-shrink-0 h-6 w-6 text-red-700';
+
+const iconMap = {
+  crow: <FaCrow className={iconProps} />,
+  discord: <FaDiscord className={iconProps} />,
+  donate: <RiHandCoinFill className={iconProps} />,
+  forums: <IoMdChatbubbles className={iconProps} />,
+  progress: <GiStoneBlock className={iconProps} />,
+  about: <FaInfoCircle className={iconProps} />,
+  apply: <FaPencilAlt className={iconProps} />,
+  merch: <FaTshirt className={iconProps} />
+};
+
+const DropdownItem = ({ children = '', href = '', description, icon }) => (
+  <a href={href} className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+    {iconMap[icon]}
+    <div className="ml-4">
+      <p className="text-base font-medium text-gray-900">{children}</p>
+      <p className="mt-1 text-sm text-gray-500">{description}</p>
+    </div>
   </a>
 );
 
@@ -34,28 +53,90 @@ export const Dropdown = ({ dropdownData }) => {
     <div className="relative">
       <button
         onClick={onClick}
-        className="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+        type="button"
+        className="group bg-white rounded-md text-gray-500 inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
         <span>{dropdownData.title}</span>
+
         <svg
-          fill="currentColor"
+          className="ml-2 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+          xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          className="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
+          fill="currentColor"
+          aria-hidden="true">
           <path
             fillRule="evenodd"
             d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-            clipRule="evenodd"></path>
+            clipRule="evenodd"
+          />
         </svg>
       </button>
       {open && (
         <div
           ref={dropdownRef}
-          className="absolute bg-white right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
-          <div className="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
-            {dropdownData.children.map((child) => (
-              <DropdownItem key={child.title} href={child.url}>
-                {child.title}
-              </DropdownItem>
-            ))}
+          className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+          <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+            <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+              {dropdownData.children.map((child) => (
+                <DropdownItem
+                  key={child.title}
+                  href={child.url}
+                  description={child.description}
+                  icon={child.icon}>
+                  {child.title}
+                </DropdownItem>
+              ))}
+            </div>
+            <div className="px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
+              <div className="flow-root">
+                <a
+                  href="/"
+                  className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100">
+                  <svg
+                    className="flex-shrink-0 h-6 w-6 text-gray-400"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                  <span className="ml-3">Watch Demo</span>
+                </a>
+              </div>
+
+              <div className="flow-root">
+                <a
+                  href="/"
+                  className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100">
+                  <svg
+                    className="flex-shrink-0 h-6 w-6 text-gray-400"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                    />
+                  </svg>
+                  <span className="ml-3">Contact Sales</span>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       )}
--- components/navbar/index.js
+++ components/navbar/index.js
@@ -6,52 +6,219 @@ import navData from './navData.json';
 export const Navbar = ({ navLinks = navData }) => {
   const [openNav, setOpenNav] = useState(false);
 
+  const toggleMobileNav = () => {
+    setOpenNav(!openNav);
+  };
+
   return (
-    <div className="w-full text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800">
-      <div className="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
-        <div className="p-4 flex flex-row items-center justify-between">
-          <a
-            href="/"
-            className="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline">
-            WesterosCraft
-          </a>
-          <button
-            onClick={() => {
-              setOpenNav(!openNav);
-            }}
-            className="md:hidden rounded-lg focus:outline-none focus:shadow-outline">
-            <svg fill="currentColor" viewBox="0 0 20 20" className="w-6 h-6">
-              {!openNav && (
+    <div className="relative bg-white z-50">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+          <div className="flex justify-start lg:w-0 lg:flex-1">
+            <a href="/">
+              <span className="sr-only">WesterosCraft</span>
+              <img className="h-6 w-auto sm:h-7" src="/westeroscraft_logo.svg" alt="" />
+            </a>
+          </div>
+          <div className="-mr-2 -my-2 md:hidden">
+            <button
+              onClick={toggleMobileNav}
+              type="button"
+              className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              <span className="sr-only">Open menu</span>
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true">
                 <path
-                  x-show="!open"
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-                  clipRule="evenodd"></path>
-              )}
-              {openNav && (
-                <path
-                  x-show="open"
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"></path>
-              )}
-            </svg>
-          </button>
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+          </div>
+          <nav className="hidden md:flex space-x-10">
+            {navLinks.map((link) =>
+              link.children && link.children.length > 1 ? (
+                <Dropdown key={link.title} dropdownData={link} />
+              ) : (
+                <Navlink key={link.title} href={link.url}>
+                  {link.title}
+                </Navlink>
+              )
+            )}
+          </nav>
         </div>
-        <nav
-          className={` ${
-            openNav ? 'flex' : 'hidden'
-          } flex-col flex-grow pb-4 md:pb-0 md:flex md:justify-end md:flex-row`}>
-          {navLinks.map((link) =>
-            link.children && link.children.length > 1 ? (
-              <Dropdown key={link.title} dropdownData={link} />
-            ) : (
-              <Navlink key={link.title} href={link.url}>
-                {link.title}
-              </Navlink>
-            )
-          )}
-        </nav>
+      </div>
+
+      <div
+        className={`absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden ${
+          openNav && 'hidden'
+        }`}>
+        <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+          <div className="pt-5 pb-6 px-5">
+            <div className="flex items-center justify-between">
+              <div>
+                <img className="h-5 w-auto" src="/westeroscraft_logo.svg" alt="WesterosCraft" />
+              </div>
+              <div className="-mr-2">
+                <button
+                  onClick={toggleMobileNav}
+                  type="button"
+                  className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="sr-only">Close menu</span>
+                  <svg
+                    className="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div className="mt-6">
+              <nav className="grid gap-y-8">
+                <a href="/" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                  <svg
+                    className="flex-shrink-0 h-6 w-6 text-red-700"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                    />
+                  </svg>
+                  <span className="ml-3 text-base font-medium text-gray-900">Analytics</span>
+                </a>
+
+                <a href="/" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                  <svg
+                    className="flex-shrink-0 h-6 w-6 text-red-700"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
+                    />
+                  </svg>
+                  <span className="ml-3 text-base font-medium text-gray-900">Engagement</span>
+                </a>
+
+                <a href="/" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                  <svg
+                    className="flex-shrink-0 h-6 w-6 text-red-700"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
+                    />
+                  </svg>
+                  <span className="ml-3 text-base font-medium text-gray-900">Security</span>
+                </a>
+
+                <a href="/" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                  <svg
+                    className="flex-shrink-0 h-6 w-6 text-red-700"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
+                    />
+                  </svg>
+                  <span className="ml-3 text-base font-medium text-gray-900">Integrations</span>
+                </a>
+
+                <a href="/" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                  <svg
+                    className="flex-shrink-0 h-6 w-6 text-red-700"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                    />
+                  </svg>
+                  <span className="ml-3 text-base font-medium text-gray-900">Automations</span>
+                </a>
+              </nav>
+            </div>
+          </div>
+          <div className="py-6 px-5 space-y-6">
+            <div className="grid grid-cols-2 gap-y-4 gap-x-8">
+              <a href="/" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                Pricing
+              </a>
+
+              <a href="/" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                Docs
+              </a>
+
+              <a href="/" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                Enterprise
+              </a>
+
+              <a href="/" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                Blog
+              </a>
+
+              <a href="/" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                Help Center
+              </a>
+
+              <a href="/" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                Guides
+              </a>
+
+              <a href="/" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                Security
+              </a>
+
+              <a href="/" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                Events
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
