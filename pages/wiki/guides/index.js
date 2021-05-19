126fb61e68898ac1a2be1bbd3c2f837995f36285
jacob.granberry <jacobgranberry@gmail.com>
2021-05-18 21:23:43 -0500

    fixes guides page again

--- pages/wiki/guides/index.js
+++ pages/wiki/guides/index.js
@@ -1,76 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { WikiLayout } from '../../../components/templates/wikiLayout';
 import { EntryCard } from '../../../components/atoms/entryCard';
 import { Flex, Text } from 'rebass';
 import Link from 'next/link';
-import { regionSlugFormatter } from '../../../utils/regionSlugFormatter';
-import { computeBreadcrumbs, camelCaseFormatter } from '../../../utils/helpers';
-import { RegionFilters } from '../../../components/atoms/regionFilters/regionFilters';
+import { computeBreadcrumbs } from '../../../utils/helpers';
 import SEO from '../../../components/organisms/seo/seo';
-import { Spinner } from '../../../components/atoms/spinner';
 import { useRouter } from 'next/router';
 import Error from 'next/error';
-import { getClient } from '../../../utils/sanity';
+import { getClient, usePreviewSubscription } from '../../../utils/sanity';
 
-// const query = `*[_type == "destination" && region == $region]`;
+const query = `*[_type == "guide"]`;
 
-const GuidesPage = () => {
+const GuidesPage = ({ preview, allGuides }) => {
   const router = useRouter();
 
-  //   if (!router.isFallback) {
-  //     return <Error statusCode={404} />;
-  //   }
+  const { data } = usePreviewSubscription(query, {
+    initialData: allGuides,
+    enabled: preview
+  });
+
+  if (!router.isFallback && !allGuides) {
+    return <Error statusCode={404} />;
+  }
 
   return (
     <>
-      {/* {data && (
-        <SEO
-          title={camelCaseFormatter(data && data[0].region) || ''}
-          description={data.pageDescription}
-          image={data.pageEntry && data.pageImage[0].url}
-        />
-      )} */}
-      <WikiLayout title={'WesterosCraft Wiki'} breadcrumb={computeBreadcrumbs(router.asPath)}>
-        <Text>Full guide list coming soon</Text>
+      {data && <SEO title={'All Guides' || ''} description={'All WesterosCraft guides'} />}
+      <WikiLayout title={'All Guides'} breadcrumb={computeBreadcrumbs(router.asPath)}>
+        <Flex flexDirection={['column', null, 'row']} flexWrap="wrap">
+          {data && data.length >= 1 ? (
+            data.map((entry) => (
+              <Link passHref href={`/wiki/guides/${entry.slug.current}`} key={entry.slug.current}>
+                <EntryCard data={entry} key={entry.slug.current} />
+              </Link>
+            ))
+          ) : (
+            <Text mt={3}>None found</Text>
+          )}
+        </Flex>
       </WikiLayout>
     </>
   );
 };
 
-// export async function getStaticPaths() {
-//   const routes = [
-//     { params: { region: 'dorne' } },
-//     { params: { region: 'riverlands' } },
-//     { params: { region: 'the-wall' } },
-//     { params: { region: 'north' } },
-//     { params: { region: 'vale' } },
-//     { params: { region: 'iron-islands' } },
-//     { params: { region: 'westerlands' } },
-//     { params: { region: 'crownlands' } },
-//     { params: { region: 'stormlands' } },
-//     { params: { region: 'reach' } },
-//     { params: { region: 'beyond-the-wall' } }
-//   ];
+export async function getStaticProps({ preview = false }) {
+  const allGuides = await getClient(preview).fetch(query);
 
-//   return {
-//     paths: routes || null,
-//     fallback: true
-//   };
-// }
-
-// export async function getStaticProps({ params = {}, preview = false }) {
-//   const { region } = params;
-
-//   const thing = regionSlugFormatter(region);
-
-//   const regionData = await getClient(preview).fetch(query, {
-//     region: thing
-//   });
-
-//   return {
-//     props: { preview, regionData },
-//     revalidate: 1
-//   };
-// }
+  return {
+    props: { preview, allGuides },
+    revalidate: 1
+  };
+}
 
 export default GuidesPage;
