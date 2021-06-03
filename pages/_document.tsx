fabc82d2adb0d8b9ff43a579b7ac105e8eded6ad
jacob.granberry <jacobgranberry@gmail.com>
2021-06-03 18:20:54 -0500

    setup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
-import {ColorModeScript} from '@chakra-ui/react';
-import Document, {Html, Head, Main, NextScript} from 'next/document';
+import { ColorModeScript } from '@chakra-ui/react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
 	render = () => (
