0ed4434b1e0d520ad003a653c6a6029ba6d36443
jacob.granberry <jacobgranberry@gmail.com>
2021-06-03 20:13:48 -0500

    dataset

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,10 +1,10 @@
-import {SanityImageSource} from '@sanity/asset-utils';
-import {createClient, createImageUrlBuilder, createPreviewSubscriptionHook} from 'next-sanity';
+import { SanityImageSource } from '@sanity/asset-utils';
+import { createClient, createImageUrlBuilder, createPreviewSubscriptionHook } from 'next-sanity';
 
 const config = {
-	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'production',
+	dataset: process.env.NODE_ENV === 'production' ? 'production' : 'develop',
 	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? 'undefined',
-	useCdn: process.env.NODE_ENV === 'production'
+	useCdn: process.env.NODE_ENV === 'production',
 };
 
 export const urlFor = (source: SanityImageSource) => createImageUrlBuilder(config).image(source);
