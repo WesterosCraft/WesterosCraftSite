613a0ddb6f766a0e5ce26ff057351a5f60b82eff
Jacob Granberry <jacob.granberry@company-corp.com>
2021-07-01 19:18:10 -0500

    adds diff env

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -2,8 +2,8 @@ import { SanityImageSource } from '@sanity/asset-utils';
 import { createClient, createImageUrlBuilder, createPreviewSubscriptionHook } from 'next-sanity';
 
 const config = {
-	dataset: process.env.NODE_ENV === 'production' ? 'production' : 'develop',
-	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? 'undefined',
+	projectId: process.env.SANITY_API_PROJECT_ID,
+	dataset: process.env.SANITY_API_DATASET,
 	useCdn: process.env.NODE_ENV === 'production',
 };
 
